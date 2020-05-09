be2722db062c1cda15ffc3446be80e7c3702c981
15910778165@163.com <15910778165@163.com>
2020-05-09 12:03:27 +0800

    Fix bugs

--- src/utils/CookieUtils.js
+++ src/utils/CookieUtils.js
@@ -6,3 +6,10 @@ export function setCookie(name, value) {
   console.log(name, value)
   document.cookie = name + "=" + escape(value) + ";path=/";
 }
+export function delCookie(name) {
+  var exp = new Date();
+  exp.setTime(exp.getTime() - 1);
+  var cval = getCookie(name);
+  if (cval != null)
+    document.cookie = name + "=" + cval + ";expires=" + exp.toGMTString();
+}
