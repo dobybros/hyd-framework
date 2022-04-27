47c92424e7cde89c1508bde53a3a0de36a54cfcd
mhm <maohaimeng@163.com>
2022-04-27 19:05:21 +0800

    fix:screenShare web

--- src/hydElectron/window/FeatureWindow.js
+++ src/hydElectron/window/FeatureWindow.js
@@ -75,6 +75,7 @@ class FeatureWindow extends HydWindow {
         // renderWindowConfig.parent = this._mainWindow
       }
       const window = new BrowserWindow(renderWindowConfig)
+      window.setContentProtection(true)
       this.showAfterCreate = showAfterCreate
       this._electronWindow = window
 
